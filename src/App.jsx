a19f65c0614ad29ca6874177672869654aa0ab5b
Adam <Adam.sturesson@live.se>
2024-12-17 09:35:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+  const [expenses, setExpenses] =  useState("");
+  const [category, setCategory] = useState("");
+  const [amount, setAmount] = useState("");
+
+  useEffect (() => {
+
+  })
+
+  const addExpense = () => {
+  }
+
+  return (
+    <div>
+      <h1>Hushållsbudget</h1>
+      
+    </div>
+  )
+}
+
+export default App
